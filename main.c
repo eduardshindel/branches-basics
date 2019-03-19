850370103f8da1af7e5519ac8c5a5a79e280e826
Шиндель Эдуард Дмитриевич <iv823s26@awp09.csc.local>
2019-03-19 12:41:15 +0700

    Edit main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int sqr(int *a)
+{
+    *a *= *a;
+    
+    return *a;
+}
+
 int main()
 {
+    int a;
+    
+    scanf("%d", &a);
+    
+    sqr(&a);
+    
+    printf("%d\n", a);
+    
     return 0;
 }
