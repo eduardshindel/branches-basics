15f14b51d31dc102cd4f76adcc662570febc8fd4
Шиндель Эдуард Дмитриевич <iv823s26@awp09.csc.local>
2019-03-19 13:13:45 +0700

    Add function sum in main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int sum(int a, int b)
+{
+    int c = a + b;
+    
+    return c;
+}
+
 int main()
 {
+    int a, b, c;
+
+    scanf("%d %d", &a, &b);
+    
+    c = sum(a, b);
+    
+    printf("%d\n", c);
+    
     return 0;
 }
